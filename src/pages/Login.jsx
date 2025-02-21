a0a2b0b7d3b1e24b746f346ef6e216f2e8b88f02
abdulrahemfaqih <faqih3935@gmail.com>
2025-02-21 14:18:04 +0700

    fix: logo in login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import icon from "../../public/icon.svg";
+import icon from "../../public/icon_login.svg";
 
 const Login = () => {
   const { login, register, authError, user, loading } = useContext(AuthContext);
@@ -33,81 +33,81 @@ const Login = () => {
   };
 
   return (
-     <div className="flex-col min-h-screen bg-white text-black flex items-center justify-center">
-      <img src={icon} className="w-32 h-32" />
-      <form
-        onSubmit={handleSubmit}
-        className="border-2 border-black bg-white p-6 rounded-md shadow-md w-80"
-      >
-        <h1 className="text-2xl font-bold mb-4">
-          {isRegister ? "Register" : "Login"}
-        </h1>
-
-        {isRegister && (
-          <div className="mb-4">
-            <label className="block mb-1 font-medium">Nama</label>
-            <input
-              type="text"
-              name="name"
-              onChange={handleChange}
-              className="w-full border-2 border-black p-2 rounded focus:outline-none"
-              required
-            />
-          </div>
-        )}
-
-        <div className="mb-4">
-          <label className="block mb-1 font-medium">Email</label>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            className="w-full border-2 border-black p-2 rounded focus:outline-none"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block mb-1 font-medium">Password</label>
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            className="w-full border-2 border-black p-2 rounded focus:outline-none"
-            required
-          />
-        </div>
-
-        {authError && (
-          <div className="mb-4 text-red-500 text-center">{authError}</div>
-        )}
-
-        {/* Loading Indicator */}
-        {loading && (
-          <div className="mb-4 text-center text-gray-600">Loading...</div>
-        )}
-
-        <button
-          type="submit"
-          disabled={loading}
-          className={`w-full border-2 border-black bg-white text-black py-2 rounded-md font-medium hover:bg-black hover:text-white transition ${
-            loading ? "opacity-50 cursor-not-allowed" : ""
-          }`}
+     <div className="flex flex-col min-h-screen bg-white text-black items-center justify-center">
+        <img src={icon} className="w-28 mb-4 " />
+        <form
+           onSubmit={handleSubmit}
+           className="border-2 border-black bg-white p-6 rounded-md shadow-md w-80"
         >
-          {isRegister ? "Register" : "Login"}
-        </button>
+           <h1 className="text-2xl font-bold mb-4">
+              {isRegister ? "Register" : "Login"}
+           </h1>
 
-        <p className="mt-4 text-center">
-          {isRegister ? "Sudah punya akun?" : "Belum punya akun?"}{" "}
-          <span
-            onClick={() => setIsRegister(!isRegister)}
-            className="text-black underline cursor-pointer hover:text-gray-700"
-          >
-            {isRegister ? "Login" : "Register"}
-          </span>
-        </p>
-      </form>
-    </div>
+           {isRegister && (
+              <div className="mb-4">
+                 <label className="block mb-1 font-medium">Nama</label>
+                 <input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    className="w-full border-2 border-black p-2 rounded focus:outline-none"
+                    required
+                 />
+              </div>
+           )}
+
+           <div className="mb-4">
+              <label className="block mb-1 font-medium">Email</label>
+              <input
+                 type="email"
+                 name="email"
+                 onChange={handleChange}
+                 className="w-full border-2 border-black p-2 rounded focus:outline-none"
+                 required
+              />
+           </div>
+
+           <div className="mb-4">
+              <label className="block mb-1 font-medium">Password</label>
+              <input
+                 type="password"
+                 name="password"
+                 onChange={handleChange}
+                 className="w-full border-2 border-black p-2 rounded focus:outline-none"
+                 required
+              />
+           </div>
+
+           {authError && (
+              <div className="mb-4 text-red-500 text-center">{authError}</div>
+           )}
+
+           {/* Loading Indicator */}
+           {loading && (
+              <div className="mb-4 text-center text-gray-600">Loading...</div>
+           )}
+
+           <button
+              type="submit"
+              disabled={loading}
+              className={`w-full border-2 border-black bg-white text-black py-2 rounded-md font-medium hover:bg-black hover:text-white transition ${
+                 loading ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+           >
+              {isRegister ? "Register" : "Login"}
+           </button>
+
+           <p className="mt-4 text-center">
+              {isRegister ? "Sudah punya akun?" : "Belum punya akun?"}{" "}
+              <span
+                 onClick={() => setIsRegister(!isRegister)}
+                 className="text-black underline cursor-pointer hover:text-gray-700"
+              >
+                 {isRegister ? "Login" : "Register"}
+              </span>
+           </p>
+        </form>
+     </div>
   );
 };
 
