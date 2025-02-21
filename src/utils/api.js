10ac23363b921f475150eff6bebe4dfd95b6d9aa
abdulrahemfaqih <faqih3935@gmail.com>
2025-02-21 11:20:23 +0700

    update api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
-import axios from 'axios';
+// src/utils/api.js
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_PUBLIC_API_URL || 'http://localhost:5000',
-})
+   baseURL: import.meta.env.VITE_PUBLIC_API_URL
+      ? `${import.meta.env.VITE_PUBLIC_API_URL}/api`
+      : "http://localhost:5000/api",
+});
 
 export default api;
