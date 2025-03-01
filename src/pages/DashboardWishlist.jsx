70ddeff33312be6cc621ffb4a656553ab64f540d
abdulrahemfaqih <faqih3935@gmail.com>
2025-03-01 16:28:45 +0700

    fix: loading delete

--- src/pages/DashboardWishlist.jsx
+++ src/pages/DashboardWishlist.jsx
@@ -105,9 +105,7 @@ const DashboardWishlist = () => {
                     </button>
                 </div>
                 {isLoadingDelete && (
-                    <div className="flex justify-center items-center">
-                        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-black"></div>
-                    </div>
+                    <p className='py-2 text-black font-semibold'>Loading..</p>
                 )}
 
                 <Wishlist items={items} onUpdate={handleUpdate} onDelete={handleDelete} />
